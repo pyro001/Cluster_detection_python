9d5256a3780d6d2f7b050eb1091e79c3e88a7c30
AgniB <agnibiswasranchi@gmail.com>
2021-12-11 20:58:42 +0100

    upgraded code
    added auto thresholding
    added  simple commenting
    added watershedding

--- backup_old/Agni_tests.py
+++ backup_old/Agni_tests.py
@@ -7,67 +7,13 @@ from scipy import ndimage
 from skimage.feature import peak_local_max
 from skimage.segmentation import watershed
 
-from oiplib import *
+from OIP21_lib_ImageProcessing_V6  import *
 
-
-def mean_class_variance(hist, thresh):
-    wf = 0.0
-    uf = 0.0
-    ub = 0.0
-    wb = 0.0
-    total = np.sum(hist)
-    # print(total)
-    if len(hist) > 256:
-        raise IndexError("object too large, recheck input")
-    for i in range(len(hist)):
-        if i < thresh:
-            wb += hist[i]
-            ub = ub + (i * hist[i])
-        elif i >= thresh:
-            wf += hist[i]
-            uf = uf + (i * hist[i])
-    if wb==0:
-        ub=0
-    else:
-        ub = ub / wb
-    if wf == 0:
-        uf=0
-    else:
-        uf = uf / wf
-    wb = wb / total
-    wf = wf / total
-
-
-
-
-    print(thresh,wb,wf,uf,ub,total)
-    variance = float(wb * wf * ((ub - uf) ** 2))
-    return (variance, thresh)
-
-
-def auto_thresh(img, mode="Thresholding"):
-    hst = hist256(img)
-    variance_list = []
-    for thresh in range(len(hst)):
-        if thresh > 1:
-            variance_list.append(mean_class_variance(hst, thresh))
-    # print(variance_list)
-    # print("MAX tuple: ", max(variance_list))
-    var, thresh = max(variance_list)
-    print("MAX tuple: ", thresh)
-    if mode == "Thresholding":
-        return threshold(img, thresh)
-    elif mode == "UpperSave":
-        img[img <= thresh] = 0
-        return img
-    elif mode == "LowerSave":
-        img[img >= thresh] = 255
-        return img
 
 
 kernel = np.ones((5, 5), np.uint8)
 if __name__ == '__main__':
-    img = cv2.imread("T001.png")
+    img = cv2.imread("./pictures/big_triangles_orginal.png")
     # img = cv2.imread("R001_001.tif")
     # img = cv2.imread("001_002.tif")
     # img = cv2.imread("SingleTCell.png")
@@ -81,8 +27,8 @@ if __name__ == '__main__':
     # gauss_img, phi, idx, idy = detect_edges(thresh)
     kernel = np.array(np.ones(3), np.uint8)
     # gauss_img, phi, idx, idy = detect_edges(gray)
-    thresh2 =  cv2.erode(cv2.dilate(thresh,kernel),kernel,iterations=1)
-    #
+    # thresh2 =  cv2.erode(cv2.dilate(thresh,kernel),kernel,iterations=1)
+    thresh2,phii=laplace_sharpen(thresh)
     # thresh2= (gauss_img)
     cv2.imshow('thresh', thresh)
     cv2.imshow('gauss', gray)
@@ -96,6 +42,8 @@ if __name__ == '__main__':
     # using 8-connectivity, then appy the Watershed algorithm
     markers = ndimage.label(localMax, structure=np.ones((3, 3)))[0]
     labels = watershed(-D, markers, mask=thresh2)
+
+    # plt.imshow("Seperated objects", labels, cmap="spectral")
     print("[INFO] {} unique segments found".format(len(np.unique(labels)) - 1))
     for label in np.unique(labels):
         # if the label is zero, we are examining the 'background'
@@ -104,7 +52,7 @@ if __name__ == '__main__':
             continue
         # otherwise, allocate memory for the label region and draw
         # it on the mask
-        mask = np.zeros(gray.shape, dtype="uint8")
+        mask = np.zeros(thresh2.shape, dtype="uint8")
         mask[labels == label] = 255
         # detect contours in the mask and grab the largest one
         cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL,
